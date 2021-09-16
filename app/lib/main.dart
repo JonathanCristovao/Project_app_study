f08065e2124772c2522db6de5a9806b74a074169
JonathanCristovao <74277960+JonathanCristovao@users.noreply.github.com>
2021-09-16 16:07:21 -0300

    first commit "begin"

---
+++ app/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Study',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Home study'),
+            centerTitle: true,
+            actions: const [
+              Padding(
+                padding: EdgeInsets.only(right: 12.0),
+                child: Icon(Icons.book),
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            child: Column(),
+          ),
+          backgroundColor: Colors.white,
+          bottomNavigationBar: BottomNavigationBar(
+              backgroundColor: Colors.grey[200],
+              onTap: (value) => debugPrint(value.toString()),
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: 'inicio',
+                  activeIcon: Icon(Icons.home),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline),
+                  label: 'Perfil',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.school_outlined),
+                  label: 'School',
+                ),
+              ]),
+          body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  MyContainer(),
+                  MyContainer(),
+                  MyContainer(),
+                ]),
+          )),
+    );
+  }
+}
